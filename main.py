11d6e7e8ccd5d32aac4c8c81226bd99f55986059
DMcmac <danielmcquitty7@gmail.com>
2025-11-26 12:31:03 -0500

    added a screen and a player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 
@@ -14,14 +15,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    refresh_clock = pygame.time.Clock()
+    dt = 0
+    danigan = Player(x= SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        danigan.draw(screen)
         pygame.display.flip()
+        dt = refresh_clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
